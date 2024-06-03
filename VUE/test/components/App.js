583357e972989b43734a79713c5dd8c2806185ef
Zac <a19860101@gmail.com>
2024-06-03 20:52:01 +0800

    20240603

---
+++ VUE/test/components/App.js
@@ -0,0 +1,18 @@
+import AppTitle from './AppTitle.js'
+import AppBtn from './AppBtn.js'
+export default {
+    template:`
+        <app-title></app-title>
+        <app-btn></app-btn>
+    `,
+    data(){
+        return {
+            title:'hello vue'       
+        } 
+    },
+    components:{
+        'app-title':AppTitle,
+        'app-btn':AppBtn
+    },
+
+}
