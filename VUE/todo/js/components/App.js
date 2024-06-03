e8adecd71a5c8970d82cb964fbab9ee3424f7413
Zac <a19860101@gmail.com>
2024-06-03 21:21:12 +0800

    20240603

---
+++ VUE/todo/js/components/App.js
@@ -0,0 +1,12 @@
+import Todos from './Todos.js'
+export default {
+    components:{
+        'todos':Todos,
+    },
+    template:`
+        <todos></todos>
+    `,
+    data(){
+        return {}
+    }
+}
---
+++ VUE/todo/js/components/Todos.js
@@ -0,0 +1,68 @@
+export default {
+    template:`
+        <form action="" @submit.prevent="addTodo()">
+            <input type="text" v-model="input">
+            <input type="submit" value="新增項目">
+        </form>
+        <h2 v-if="filter.todoUnCompleted.length">未完成 ({{filter.todoUnCompleted.length}})</h2>
+        <ul>
+            <li v-for="todo in filter.todoUnCompleted" :key="todo.id">
+                {{todo.name}}
+                <input type="checkbox" v-model="todo.isCompleted">
+            </li>
+        </ul>
+        <h2 v-if="filter.todoCompleted.length">已完成 ({{filter.todoCompleted.length}})</h2>
+        <ul>
+            <li v-for="todo in filter.todoCompleted" :key="todo.id">
+                {{todo.name}}
+                <input type="checkbox" v-model="todo.isCompleted">
+            </li>
+        </ul>
+    `,
+    data(){
+        return {
+            input:'',
+            todos:[
+                {
+                    id:1,/* 主鍵 */
+                    name:'洗衣服',
+                    isCompleted:false,
+                },
+                {
+                    id:2,
+                    name:'做報告',
+                    isCompleted:false,
+                },
+                {
+                    id:3,
+                    name:'運動',
+                    isCompleted:true,
+                },
+            ]
+        }
+    },
+    methods:{
+        addTodo(){
+            this.todos.push({
+                id:Date.now(),
+                name:this.input,
+                isCompleted:false,
+            });
+            this.input='';
+        }
+    },
+    computed:{
+        filter(){
+            return {
+                todoCompleted:this.todos.filter(data=>data.isCompleted === true),
+                todoUnCompleted:this.todos.filter(data=>data.isCompleted === false)
+            }
+        },
+        todoCompleted(){
+            return this.todos.filter(data=>data.isCompleted === true)
+        },
+        todoUnCompleted(){
+            return this.todos.filter(data=>data.isCompleted === false)
+        }
+    }
+}
