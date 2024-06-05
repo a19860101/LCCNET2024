89e8d799471d4b6d0aa9d64627699f3044d042e7
Zac <a19860101@gmail.com>
2024-06-05 21:43:54 +0800

    20240605

---
+++ vite/src/App.vue
@@ -0,0 +1,27 @@
+
+<script>
+  export default {
+    data(){
+      return {
+        title: 'Hello Vue 123'
+      }
+    },
+    methods:{
+      test(){
+        alert('hello methods');
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <a href="#" @click="test()">click</a>
+</template>
+
+
+<style>
+  h1 {
+    color: #f00;
+  }
+</style>
