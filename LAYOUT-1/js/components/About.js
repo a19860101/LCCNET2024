efd405c6aa9a4cc4eee48e794663dface9244182
Zac <a19860101@gmail.com>
2024-06-05 20:52:05 +0800

    20240605

---
+++ LAYOUT-1/js/components/About.js
@@ -0,0 +1,25 @@
+export default {
+    template:`
+    <section id="about">
+        <div class="container">
+            <div class="about-img">
+                <div class="about-img-inner">
+                    <img src="https://picsum.photos/id/98/600" alt="">
+                </div>
+            </div>
+            <div class="about-content">
+                <h2>Hello , I am zxzxcxzc</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi optio cupiditate quisquam ut laborum, exercitationem facere perferendis sequi quas tempore fuga, tempora, temporibus molestiae nisi odit ab earum accusantium explicabo!
+                </p>
+                <ul>
+                    <li>平面設計</li>
+                    <li>介面設計</li>
+                    <li>前端開發</li>
+                    <li>影片剪輯</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    `
+}
---
+++ LAYOUT-1/js/components/Folio.js
@@ -0,0 +1,33 @@
+export default {
+    template:`
+    <section id="folio">
+        <h2>Folio</h2>
+        <div class="container">
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/10/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/20/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/30/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/40/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/50/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/60/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/70/800/600" alt="">
+            </div>
+            <div class="folio-col">
+                <img src="https://picsum.photos/id/80/800/600" alt="">
+            </div>
+        </div>
+    </section>
+    `
+}
