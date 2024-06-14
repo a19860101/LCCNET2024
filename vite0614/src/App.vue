d93d4f9cbc6d9c3d6fc6a9ad5bd0df68212bcc40
Zac <a19860101@gmail.com>
2024-06-14 20:29:34 +0800

    20240614

--- vite0614/src/App.vue
+++ vite0614/src/App.vue
@@ -9,9 +9,22 @@
 <template>
   <Nav></Nav>
   <Header></Header>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+  <transition>
+    <component :is="Component" />
+  </transition>
+</router-view>
 </template>
 
 <style>
+  .v-enter-active,
+  .v-leave-active {
+    transition: all 0.5s ease;
+  }
 
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+    transform: translateY(100px)
+  }
 </style>
--- vite0614/src/route.js
+++ vite0614/src/route.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import Home from './components/Home.vue'
 import About from './components/About.vue'
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
